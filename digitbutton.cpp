32120ba9035350e269ced65d6df0b03552b04a1c
john chang <john.chang20019@gmail.com>
2025-03-10 17:50:03 +0800

    update the keyboard function

--- digitbutton.cpp
+++ digitbutton.cpp
@@ -13,7 +13,70 @@ digitButton::~digitButton()
     delete ui;
 }
 
-void digitButton::on_pushButton_2_clicked()
+void digitButton::on_back_clicked()
 {
     this->close();
 }
+
+void digitButton::on_ok_clicked()
+{
+    this->close();
+}
+
+void digitButton::on_clear_clicked()
+{
+    ui->input_area->clear();
+}
+
+void digitButton::on_zero0_clicked()
+{
+    ui->input_area->insert("0");
+}
+
+void digitButton::on_one1_clicked()
+{
+    ui->input_area->insert("1");
+}
+
+void digitButton::on_two2_clicked()
+{
+    ui->input_area->insert("2");
+}
+
+void digitButton::on_three3_clicked()
+{
+    ui->input_area->insert("3");
+}
+
+void digitButton::on_four4_clicked()
+{
+    ui->input_area->insert("4");
+}
+
+void digitButton::on_five5_clicked()
+{
+    ui->input_area->insert("5");
+}
+
+void digitButton::on_six6_clicked()
+{
+    ui->input_area->insert("6");
+}
+
+void digitButton::on_seven7_clicked()
+{
+    ui->input_area->insert("7");
+}
+
+void digitButton::on_eight8_clicked()
+{
+    ui->input_area->insert("8");
+}
+
+void digitButton::on_nine9_clicked()
+{
+    ui->input_area->insert("9");
+}
+
+
+
--- digitbutton.h
+++ digitbutton.h
@@ -16,7 +16,20 @@ public:
     ~digitButton();
 
 private slots:
-    void on_pushButton_2_clicked();
+    void on_back_clicked();
+    void on_ok_clicked();
+    void on_clear_clicked();
+    void on_zero0_clicked();
+    void on_one1_clicked();
+    void on_two2_clicked();
+    void on_three3_clicked();
+    void on_four4_clicked();
+    void on_five5_clicked();
+    void on_six6_clicked();
+    void on_seven7_clicked();
+    void on_eight8_clicked();
+    void on_nine9_clicked();
+
 
 private:
     Ui::digitButton *ui;
